b17f02a8a3f5abc78b3c91a3583e9953c1f91cc9
Sergey Kovalchuk <81366846+SergeyKovalchuk2086@users.noreply.github.com>
2023-03-02 14:29:12 +0700

    test

--- src/components/ui/BaseInput/index.vue
+++ src/components/ui/BaseInput/index.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="form-input">
-        <input class="input" 
+        <input class="input"
+             v-model="val"
             :type="props.type"
             :name="props.name"
             :id="props.name"
             :placeholder="props.placeholder"
-            :value="props.value"
-            @input="updateValue"
+             :checked="props.value"
 
         >
         <label :for="props.name">{{ props.label }}</label>
@@ -21,36 +21,39 @@
 }
 </style>
 <script setup>
-import { defineProps, defineEmits, ref, computed} from 'vue';
+import { defineProps, defineEmits, computed } from 'vue'
 
-//---------------------------------------------------
-// const emits = defineEmits(['update:value']);
-// const props = defineProps({
-//     value: {
-//         type: String,
-//         default: ''
-//     },
-//     name: {
-//         type: String,
-//         required: true
-//     },
-//     type: {
-//         type: String,
-//         default: 'text'
-//     },
-//     placeholder: {
-//         type: String,
-//         required: true
-//     },
-//     label: {
-//         type: String,
-//         required: true
-//     }
-// })
-// const updateValue = (event) => {
-//     emits('update:value', event.target.value);
-// }
+const emits = defineEmits([ 'update:value' ])
+const props = defineProps({
+    value: {
+        type: Boolean,
+        default: false
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        default: 'text'
+    },
+    placeholder: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        required: true
+    }
+})
 
-//---------------------------------------------------
+const val = computed({
+  get () {
+    return props.value
+  },
+  set (value) {
+    emits('update:value', value, props.name)
+  }
+})
 
 </script>
--- src/pages/seguros/index.vue
+++ src/pages/seguros/index.vue
@@ -9,8 +9,14 @@
   />
   <Advantages />
   <Questions />
-
-  
+  <BaseInput
+    :value="v"
+    name="test"
+    type="checkbox"
+    placeholder="test plh"
+    label="label test"
+    @update:value="(val, n) => update(val, n)"
+  />
 </template>
 <style scoped>
 .mt {
@@ -19,8 +25,16 @@
 
 </style>
 <script setup>
+import { BaseInput } from '../../components/ui'
 import { HeaderInner, Tarif, Advantages, Questions } from '../../components/widgets'
+import { ref } from 'vue'
 
+const v = ref(true)
 
+function update (value, name) {
+  console.log('value :', value, name)
+  console.log(typeof value)
+  v.value = value
+}
 
 </script>
