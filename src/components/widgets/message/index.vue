bb86bf69b900b2981405829c3cae9c04a945844c
Pogopin <chegarakafree@gmail.com>
2023-03-06 11:58:31 +0300

    add form/location

---
+++ src/components/widgets/message/index.vue
@@ -0,0 +1,204 @@
+<template>
+    <section class="send-message">
+        <div class="send-message__container">
+            <div class="contacts__item item">
+                <h4 class="item__title">Loja Online</h4>
+                <div class="item__registration">
+                    <ul class="item__registration-list">
+                        <li>Rua Ali Perto, 35</li>
+                        <li>Rio de Janeiro - RJ</li>
+                        <li>Brasil - Terra - Vila Láctea</li>
+                    </ul>
+                </div>
+                <div class="item__email">
+                    <ul class="item__email-list">
+                        <li>vendas@bikcraft.com</li>
+                        <li>assistencia@bikcraft.com</li>
+                    </ul>
+                </div>
+                <p class="item__phone">+55 22 99999-9999</p>
+                <div class="item__social">
+                    <img src="@/assets/img/icon/instagramY.svg" alt="instagram">
+                    <img src="@/assets/img/icon/youtubeY.svg" alt="youtube">
+                    <img src="@/assets/img/icon/facebookY.svg" alt="facebook">
+                </div>
+            </div>
+            <div class="form-message__item">
+                <form class="form">
+                    <div class="form__input">
+                        <div class="input__block">
+                            <BaseInput
+                                label="Nome"
+                                name="inName"
+                                placeholder="Seu nome"
+                                type="text"
+                                @update:value="(val) => formData.name = val"
+                            />
+                        </div>
+                        <div class="input__block">
+                            <BaseInput                                 
+                                label="Telefone"
+                                name="inTelefone"
+                                placeholder="(22) 99999-9999"
+                                type="text"
+                                @update:value="(val) => formData.telephone = val"
+                            />
+                        </div>
+                    </div>
+                    <div class="input__block">
+                        <BaseInput                                 
+                            label="Email"
+                            name="inEmail"
+                            placeholder="contato@email.com"
+                            type="text"
+                            @update:value="(val) => formData.email = val"
+                        />
+                    </div>
+                    <div class="input__block">
+                        <BaseInput                                 
+                            label="Mensagem"
+                            name="message-text"
+                            placeholder="O que você precisa?"
+                            type="textarea"
+                            @update:value="(val) => formData.message = val"
+                        />
+                    </div>
+                    <div class="form__button">
+                        <BaseButton
+                            title="ENVIAR MENSAGEM"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+</template>
+<style scoped>
+/* 
+.block {
+    flex-basis: 100%;
+} */
+.form {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+.input-name {
+    position: relative;
+    padding: 12px;
+    background-color: var(--tree-text-color);
+    border: 1px solid #EDEDED;
+    border-radius: 5px;
+    width: 100%;
+}
+.label {
+    display: block;
+    margin-bottom: 4px;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    color: var(--bg-second-color);
+}
+.form__input {
+    display: flex;
+    gap: 20px;
+    
+}
+.input__block {
+    flex-basis: 100%;
+}
+.send-message {
+    margin-bottom: 120px;
+}
+.contacts__item {
+    flex-grow: 1;
+    max-width: 460px;
+    padding: 60px 0 150px 60px;
+    color: var(--text-color);
+    background-color: var(--bg-color);
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+    box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+}
+.item__title {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 600;
+    margin-bottom: 40px;
+}
+.item__registration {
+    position: relative;
+    margin-bottom: 32px;
+    padding-left: 31px;
+}
+
+.item__registration::before {
+    position: absolute;
+    content: '';
+    background-image: url('@/assets/img/icon/local.svg');
+    width: 22px;
+    height: 19px;
+    left: 0;
+    background-repeat: no-repeat;
+}
+.item__email {
+    position: relative;
+    margin-bottom: 32px;
+    padding-left: 31px;
+}
+.item__email::before {
+    position: absolute;
+    content: '';
+    background-image: url('@/assets/img/icon/email.svg');
+    width: 22px;
+    height: 19px;
+    left: 0;
+    background-repeat: no-repeat;
+}
+.item__phone {
+    position: relative;
+    margin-bottom: 60px;
+    padding-left: 31px;
+}
+.item__phone::before {
+    position: absolute;
+    content: '';
+    background-image: url('@/assets/img/icon/telefone.svg');
+    width: 22px;
+    height: 19px;
+    left: 0;
+    background-repeat: no-repeat;
+}
+.item__social {
+    display: flex;
+    gap: 32px;
+    margin-left: 32px;
+}
+.send-message__container {
+    display: flex;
+    /* justify-content: space-between; */
+}
+.form-message__item {
+    flex-grow: 1;
+    padding: 60px 30px 60px 60px;
+    box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+}
+</style>
+<script setup>
+import { BaseButton, BaseInput } from '../../ui/index';
+import { ref } from 'vue';
+const formData = ref({
+    name: '',
+    telephone: '',
+    email: '',
+    message: ''
+})
+// function inp(value) {
+//     console.log('имя', value);
+    
+// }
+
+</script>
