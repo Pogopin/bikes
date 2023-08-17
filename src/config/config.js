4765f78f816412940974acaf75a52334a45171b0
Sergey Kovalchuk <81366846+SergeyKovalchuk2086@users.noreply.github.com>
2023-08-17 18:09:02 +0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,16 @@
+import bicicleta1 from '@/assets/img/bicicleta1.jpg'
+import bicicleta2 from '@/assets/img/bicicleta2.jpg'
+import bicicleta3 from '@/assets/img/bicicleta3.jpg'
+
 export const bikes = [
-    { id: 1, img: 'bicicleta1.jpg', name: 'Magic Migth', price: 'R$ 2499' },
-    { id: 2, img: 'bicicleta2.jpg', name: 'Nimbus Stark', price: 'R$ 4909' },
-    { id: 3, img: 'bicicleta3.jpg', name: 'Nebula Cosmic', price: 'R$ 3999' }
+    { id: 1, img: bicicleta1, name: 'Magic Migth', price: 'R$ 2499' },
+    { id: 2, img: bicicleta2, name: 'Nimbus Stark', price: 'R$ 4909' },
+    { id: 3, img: bicicleta3, name: 'Nebula Cosmic', price: 'R$ 3999' }
 ]
+
+
+// export const bikes = [
+//     { id: 1, img: 'bicicleta1.jpg', name: 'Magic Migth', price: 'R$ 2499' },
+//     { id: 2, img: 'bicicleta2.jpg', name: 'Nimbus Stark', price: 'R$ 4909' },
+//     { id: 3, img: 'bicicleta3.jpg', name: 'Nebula Cosmic', price: 'R$ 3999' }
+// ]
